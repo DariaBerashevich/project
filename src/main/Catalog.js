3546aa50a80355fa97aa882cae51eb8398728306
Dasha Berashevich <dasha2013724@gmail.com>
2021-04-19 15:59:07 +0300

    add changes

--- src/main/Catalog.js
+++ src/main/Catalog.js
@@ -1,27 +1,29 @@
 import React, { useState } from "react";
 
+const url = "https://api.punkapi.com/v2/beers";
+
 function Catalog() {
   let data;
-  const [state, setstate] = useState([]);
+  const [beerList, setBeerList] = useState([]);
   let xhttp = new XMLHttpRequest();
   xhttp.onload = function () {
     if (this.readyState === 4 && this.status === 200) {
       data = JSON.parse(xhttp.responseText);
     }
-    setstate(data);
+    setBeerList(data);
   };
-  xhttp.open("GET", "https://api.punkapi.com/v2/beers", true);
+  xhttp.open("GET", url, true);
   xhttp.send(null);
 
   return (
     <div className="catalog">
-      {state.map((obj) => {
+      {beerList.map(({ id, image_url, tagline, name }) => {
         return (
-          <div className="catalog__card card" key={obj.id}>
-            <img className="card__image" src={obj.image_url} alt="Beer logo" />
+          <div className="catalog__card card" key={id}>
+            <img className="card__image" src={image_url} alt="Beer logo" />
             <div className="card__description">
-              <h2 className="card__beer-name">{obj.name}</h2>
-              <h4 className="card__beer-description">{obj.tagline}</h4>
+              <h2 className="card__beer-name">{name}</h2>
+              <h4 className="card__beer-description">{tagline}</h4>
               <div>
                 <button>Open</button>
                 <button>Favorite</button>
