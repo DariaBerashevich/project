22a8ca955846b6965632efd2e710c1d8354964d8
Dasha Berashevich <dasha2013724@gmail.com>
2021-04-20 12:09:12 +0300

    add burger-menu and infinite scroll

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,8 +1,48 @@
 import React from "react";
+import menu from "../../src/assets/menu.png";
 
 function Header() {
+  let menuHandler = () => {
+    let burgerMenu = document.getElementsByClassName("list")[0];
+    if (burgerMenu.classList.contains("hide")) {
+      burgerMenu.classList.remove("hide");
+      burgerMenu.classList.add("show");
+    }
+  };
+
+  document.onclick = function (e) {
+    let burgerMenu = document.getElementsByClassName("list")[0];
+    if (
+      e.target.className !== "burger-menu__icon" &&
+      e.target.classList.contains("burger-menu__list") === false &&
+      e.target.classList.contains("list__title") === false
+    ) {
+      console.log(e.target.className);
+      if (burgerMenu.classList.contains("show")) {
+        burgerMenu.classList.remove("show");
+        burgerMenu.classList.add("hide");
+      }
+    }
+  };
+
   return (
     <header className="header">
+      <div className="header__burger-menu burger-menu">
+        <img
+          className="burger-menu__icon"
+          src={menu}
+          alt="Burger menu icon"
+          onClick={menuHandler}
+        />
+
+        <ul className="burger-menu__list list hide">
+          <h2 className="list__title">Beer catalog</h2>
+          <li id="list" className="list__item list__item-home">
+            Home
+          </li>
+          <li className="list__item list__item-favorites">Favorites</li>
+        </ul>
+      </div>
       <h1 className="header__title">Beer catalog</h1>
     </header>
   );
--- src/main/Catalog.js
+++ src/main/Catalog.js
@@ -1,37 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import SearchForm from "./SearchForm";
 
-const url = "https://api.punkapi.com/v2/beers";
+const beersUrl = `https://api.punkapi.com/v2/beers`;
 
 function Catalog() {
-  let data;
   const [beerList, setBeerList] = useState([]);
-  let xhttp = new XMLHttpRequest();
-  xhttp.onload = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      data = JSON.parse(xhttp.responseText);
-    }
-    setBeerList(data);
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send(null);
+  const [beerNumber, setBeerNumber] = useState(9);
+
+  useEffect(() => {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        setBeerList(JSON.parse(xhttp.responseText));
+      }
+    };
+    xhttp.open("GET", `${beersUrl}?per_page=${beerNumber}`, true);
+    xhttp.send(null);
+  }, [beerNumber]);
 
   return (
-    <div className="catalog">
-      {beerList.map(({ id, image_url, tagline, name }) => {
-        return (
-          <div className="catalog__card card" key={id}>
-            <img className="card__image" src={image_url} alt="Beer logo" />
-            <div className="card__description">
-              <h2 className="card__beer-name">{name}</h2>
-              <h4 className="card__beer-description">{tagline}</h4>
-              <div>
-                <button>Open</button>
-                <button>Favorite</button>
+    <div className="catalog-page">
+      <SearchForm />
+      <InfiniteScroll
+        dataLength={beerList.length}
+        next={setTimeout(() => setBeerNumber(beerNumber + 9), 2500)}
+        hasMore={true || false}
+        loader={<br />}
+        className="catalog"
+      >
+        {beerList.map(({ id, image_url, tagline, name }) => {
+          return (
+            <div className="catalog__card card" key={id}>
+              <img className="card__image" src={image_url} alt="Beer logo" />
+              <div className="card__description">
+                <h2 className="card__beer-name">{name}</h2>
+                <h4 className="card__beer-description">{tagline}</h4>
+                <div>
+                  <button>Open</button>
+                  <button>Favorite</button>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </InfiniteScroll>
     </div>
   );
 }
